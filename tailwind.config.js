6dce81362926304c1fc59ba49a54604cb405357d
ataher83 <taher83@gmail.com>
2024-01-22 14:38:49 +0600

    banner section with responsive completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        colors: {},
+        backgroundImage: {
+          'banner-image':
+            "linear-gradient(to right bottom, rgba(43, 108, 176, 0.9), rgba(43, 108, 176, 0.9)), url('images/banner.png')",
+        },
+      },
     },
     plugins: [],
   }
